832b4978f77389bd14cf6fe76f893e61ecc09e18
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2022-02-21 09:26:42 +0100

    Design Kalender
    Kalender besser gestaltet und erste Versuche in Richtung Synchronisation von Kalender und Ereignissen

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -16,10 +16,23 @@ export class CalendarComponent implements OnInit {
   }
 
   calendarOptions: CalendarOptions = {
-    initialView: 'dayGridMonth'
-    
+    initialView: 'dayGridMonth',
+    //dateClick: this.handleDateClick.bind(this),
+    events: [
+      { title: 'event 1', date: '2022-02-05' },
+      { title: 'event 2', date: '2019-02-04' }
+    ]
   };
 
+  /*
+  handleDateClick(arg: { dateStr: string; }) {
+    alert('date click! ' + arg.dateStr)
+  }
+*/
 
+async syncCalendar (){
+
+  
+}
 
 }
