c6a74327711a96a19b95e27813537badf1bf616f
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2022-02-21 10:07:46 +0100

    Termine eintragen
    Items lassen sich in den Kalender eintragen. Diese Methode muss im nächsten Schritt so abgewandelt werden, dass Sie automatisch die bestehenden Elemente der Datenbank einträgt

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CalendarOptions } from '@fullcalendar/angular';
-
+import { CalendarOptions, DateSelectArg, EventClickArg, EventApi } from '@fullcalendar/angular';
+import { Todo } from '../todo';
+import { TodoService } from '../todo.service';
+import { createEventId } from './event-ulits';
 
 
 @Component({
@@ -9,30 +11,45 @@ import { CalendarOptions } from '@fullcalendar/angular';
   styleUrls: ['./calendar.component.css']
 })
 export class CalendarComponent implements OnInit {
-
-  constructor() { }
+  todos: Todo[] = [];
+  constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
   }
 
   calendarOptions: CalendarOptions = {
     initialView: 'dayGridMonth',
-    //dateClick: this.handleDateClick.bind(this),
+    weekends: true,
+    editable: true,
+    selectable: true,
+    selectMirror: true,
+    dayMaxEvents: true,
+    select: this.handleDateSelect.bind(this),
     events: [
       { title: 'event 1', date: '2022-02-05' },
       { title: 'event 2', date: '2019-02-04' }
     ]
   };
 
-  /*
-  handleDateClick(arg: { dateStr: string; }) {
-    alert('date click! ' + arg.dateStr)
+
+
+handleDateSelect(selectInfo: DateSelectArg) {
+  const title = prompt('Please enter a new title for your event');
+  const calendarApi = selectInfo.view.calendar;
+
+  calendarApi.unselect(); // clear date selection
+
+  if (title) {
+    calendarApi.addEvent({
+      id: createEventId(),
+      title,
+      start: selectInfo.startStr,
+      end: selectInfo.endStr,
+      allDay: selectInfo.allDay
+    });
   }
-*/
-
-async syncCalendar (){
-
+}
   
 }
 
-}
+
