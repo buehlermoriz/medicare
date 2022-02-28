8c2821c1a33d00d47fc6070ad5fbcab755a51786
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2022-02-28 10:09:44 +0100

    update Name und Steigerung des Applimits

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'my-pwa'`, () => {
+  it(`should have as title 'medicare'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('my-pwa');
+    expect(app.title).toEqual('medicare');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-pwa app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('medicare app is running!');
   });
 });
