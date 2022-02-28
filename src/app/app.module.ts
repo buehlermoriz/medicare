a1e194daa7076a9e715c668b295bddc06fea2dcf
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2022-02-28 13:02:30 +0100

    Funktion Wochentage
    fehlerhafte Funktion zur Erstellung mehrerer Einträge
    Fehler:
    1. Montag geht nicht
    2. Liste immer ein Element zu kurz
    3. Loop hängt?
    4. if anweisungen werden nur einmal überprüft

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -29,6 +29,7 @@ import listPlugin from '@fullcalendar/list';
 import interactionPlugin from '@fullcalendar/interaction';
 import { UpdateComponent } from './update/update.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import * as moment from 'moment';
 
 FullCalendarModule.registerPlugins([
   dayGridPlugin,
@@ -75,6 +76,7 @@ FullCalendarModule.registerPlugins([
     BrowserModule,
     FormsModule,
     ReactiveFormsModule,
+  
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -3,6 +3,7 @@ import { Todo } from '../todo';
 import { TodoService } from '../todo.service';
 import {FormGroup, FormControl, FormsModule, ReactiveFormsModule} from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import * as moment from 'moment';
 
 
 
@@ -39,14 +40,52 @@ export class TodosComponent implements OnInit {
    consumption_midday : boolean,
    consumption_evening : boolean){
     
-     var startDate = this.range.get("start")?.value
-     var endDate = this.range.get("end")?.value
-    
-     for(var day = startDate; day <= endDate; day.setDate){
+     var startDate = this.range.get("start")?.value;
+    //  var startDateFormated = (moment(startDate)).format('DD-MMM-YYYY')
 
-     }
+     var endDate = this.range.get("end")?.value;
+    //  var endDateFormated = (moment(endDate)).format('DD-MMM-YYYY')
 
-    console.log(this.range.get("start")?.value);
+
+     for(var day = startDate; day <= endDate; day.setDate(day.getDate() +1)){
+      var consumption_start = day;
+      var consumption_end = day;
+      if(consumption_monday===true && day.getDate()===0){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Montag Success")
+      }
+      if(consumption_tuesday===true && day.getDate()===1){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Dienstag Success")
+      }
+      if(consumption_wednesday===true && day.getDate()===2){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Mittwoch Success")
+      }
+      if(consumption_thirsday===true && day.getDate()===3){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Donnerstag Success")
+      }
+      if(consumption_friday===true && day.getDate()===4){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Freitag Success")
+      }
+      if(consumption_satturday===true && day.getDate()===5){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Samstag Success")
+      }
+      if(consumption_sunday===true && day.getDate()===6){
+        await this.todoService.add(medicine, description, consumption_start, consumption_end, consumption_monday, consumption_tuesday, consumption_wednesday, consumption_thirsday, consumption_friday, consumption_satturday, consumption_sunday, consumption_morning, consumption_midday, consumption_evening );
+        console.log("Sonntag Success")
+      }
+      // else{
+      //   alert("Bitte wählen Sie einen Wochentag aus!")
+      //   break
+      // }
+      console.log("Tag "+ day);
+      }
+      await this.loadTodos();
+  console.log("test")
 
 
   }
