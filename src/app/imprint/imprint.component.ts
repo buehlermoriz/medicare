1c5e9ec27958265f97a1a39b3fe544ce4478e00d
Jona Wölfl <jona.woelfl@gmx.net>
2022-03-18 16:22:59 +0100

    Imprint Update

--- src/app/imprint/imprint.component.ts
+++ src/app/imprint/imprint.component.ts
@@ -8,12 +8,20 @@ import { TodoService } from '../todo.service';
   styleUrls: ['./imprint.component.css']
 })
 export class ImprintComponent implements OnInit {
-
-  constructor(private todoService: TodoService) { }
+     breakpoint: number | undefined;
+     height!: string | number;
+  constructor(private todoService: TodoService) { 
+   
+  }
 
   ngOnInit(): void {
     this.todoService.syncCalendar();
-
+    this.breakpoint = (window.innerWidth <= 1090) ? 1 : 2;    
+    this.height = (window.innerWidth <= 1090) ? "400px" : "500px";
   }
 
+  onResize(event: any) {
+    this.breakpoint = (event.target.innerWidth <= 1090) ? 1 : 2;
+    this.height = (event.target.innerWidth <= 1090) ? "400px" : "500px";
+  }
 }
