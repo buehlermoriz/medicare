0b7b12e89b07c5a06e2375551f25d63b525d4e03
Moriz Bühler <80971264+buehlermoriz@users.noreply.github.com>
2022-03-18 16:07:10 +0100

    update 1

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -23,14 +23,7 @@ export class AboutComponent implements OnInit {
     this.todoService.syncCalendar();
    
   }
-  /*
-  tiles: Tile[] = [
-    {text: 'Hallo Das sind Wir.', cols: 3, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
-  ];
-  */
+  
   }
 
   
